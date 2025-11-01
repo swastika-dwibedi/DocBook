1a4f2d6e87740bdbf1db1c750558dff166e93d1a
Swastika <swastikadwibedi03@gmail.com>
2025-11-02 02:12:55 +0530

    add

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <p>© 2025 DocBook. All Rights Reserved.</p>
+  </footer>
+);
+
+export default Footer;
