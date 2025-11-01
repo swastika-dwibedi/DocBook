126331907c2bbc2e50e8808ae635798d696b611d
Swastika <swastikadwibedi03@gmail.com>
2025-11-02 03:28:22 +0530

    delete

--- src/api/doctorService.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_BASE = "https://dummyjson.com/users"; // temporary API
-
-export const getDoctors = async () => {
-  const response = await axios.get(API_BASE);
-  return response.data.users.slice(0, 6); // 6 sample doctors
-};
